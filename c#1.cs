d576e5a7f4436129a9ebf43efea131ce656e2feb
Lawrance <rozario.lawrance@capgemini.com>
2021-04-26 16:12:48 +0530

    first commit

---
+++ c#1.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+
+class Program
+{
+  public static void sum(double a, double b)
+  {
+    Console.WriteLine("sum of a + b is "+ (a +b)); 
+    
+  }
+  
+  
+  
+   public static void Main()
+    {
+        
+        Program.sum(1.5,2.5);
+    }
+}
---
+++ c#4.cs
@@ -0,0 +1,17 @@
+Console.WriteLine("Enter a sentence");
+            String s = Console.ReadLine();
+            char[] arr;
+            arr = s.ToCharArray();
+            for(int i=0; i<arr.Length;i++)
+            {
+                if (Char.IsLower(arr[i]))
+                {
+                    Console.Write(Char.ToUpper(arr[i]));
+                }
+                else
+                {
+                    Console.Write(Char.ToLower(arr[i]));
+                }                               
+            }
+            Console.ReadLine();
+        }
